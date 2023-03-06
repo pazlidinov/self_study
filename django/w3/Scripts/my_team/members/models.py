f59a6d9f0e3d1036596535b8e0196329abe8ed30
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-03-06 17:14:06 +0500

    create w3 project

---
+++ django/w3/Scripts/my_team/members/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Members(models.Model):
+    first_name=models.CharField(max_length=150)
+    last_name=models.CharField(max_length=150)
+    phone=models.IntegerField(null=True)
+    joined_date=models.DateField(null=True)
