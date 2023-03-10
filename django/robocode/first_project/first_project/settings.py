0f0d55f28cd14d73a322a19f79208c47c87f1402
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-03-10 09:21:42 +0500

    add templates

--- django/robocode/first_project/first_project/settings.py
+++ django/robocode/first_project/first_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ django/robocode/first_project/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def homePageView(request):
+    return render(request, 'index.html')
+
+def aboutPageView(request):
+    return render(request, 'about.html')
+
+def contactPageView(request):
+    return render(request, 'contact.html')
