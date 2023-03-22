c5fd1b48024a1dbcfe451ffc507ecb1dc0e515dc
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-03-22 13:24:58 +0500

    create filter

---
+++ django/robocode/db_managment/main_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import Stacks, Course, Student
+# Create your views here.
+
+def testView(request):
+    return render(request, "index.html")
+
+def search(request):
+    # your db managment is here
+
+    # first_stack=Stacks.objects.first()
+    # last_stack=Stacks.objects.last()
+    
+    # print(first_stack)
+    # print(last_stack)
+
+    # get
+    # one_item=Student.objects.get(name='bekzod')
+    # print(one_item)  #faqat bitta elmentchiqaradi
+    if request.method =="GET":
+        query=request.GET.get("q")
+        data_stacks=Stacks.objects.filter(name__icontains=query)
+        data_course=Course.objects.filter(name__icontains=query)
+        data_students=Student.objects.filter(name__icontains=query)
+       
+        if (data_stacks or data_students or data_course) and query!='':
+            return render(request, 
+                        'index.html', 
+                        context={"students":data_students,
+                                  "course":data_course,
+                                  "stacks":data_stacks})
+        else:
+            return render(request, 'index.html', context={'message':'Result not found'})
+
+    return render(request, "index.html")
