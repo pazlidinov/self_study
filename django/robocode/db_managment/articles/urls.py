8a1ce9a40b9ab4452bf5a34d371b637a71faff55
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-03-30 15:09:09 +0500

    remote and add

--- django/robocode/db_managment/articles/urls.py
+++ django/robocode/db_managment/articles/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .import views
+
+from . import views
 
 app_name = 'articles'
 
 urlpatterns = [
     path('', views.all_articles_view, name='post_list'),
-    
+     path('detail/<slug:article_slug>', views.post_detail, name='detail'),
+    path("category/<slug:category_slug>/",
+         views.category_list, name='category_list')
 ]
+
 
--- django/robocode/db_managment/articles/views.py
+++ django/robocode/db_managment/articles/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 
+from .models import *
+from .utils import check_article_view
+
 # Create your views here.
 
+
 def all_articles_view(request):
-    return render(request, 'articles/posts.html')
+    # request.session.clear()
+    # print(request.session.get("read_articles"))
+
+    all_categories = Category.objects.all()
+    all_posts = Article.objects.all()
+
+    data = {
+        'posts': all_posts,
+        'categories': all_categories
+    }
+
+    return render(request, "articles/posts.html", context=data)
+
+
+def post_detail(request, article_slug):
+    article = Article.objects.get(slug=article_slug)
+
+    if check_article_view(request, article.id):
+        article.views += 1
+        article.save()
+    else:
+        pass
+
+    return render(request, 'articles/detail.html', context={"object": article})
+
+
+def category_list(request, category_slug):
+    category = Category.objects.get(slug=category_slug)
+    articles = Article.objects.filter(category=category)
+    return render(request, 'articles/category_posts.html', context={"posts": articles})
+    # comm
