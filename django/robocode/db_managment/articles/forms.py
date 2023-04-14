f487d1c68d134f73afba4573281c1b8848b98b73
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-04-14 11:16:10 +0500

    complete forms

--- django/robocode/db_managment/articles/forms.py
+++ django/robocode/db_managment/articles/forms.py
@@ -10,7 +10,7 @@ class AddArticleForm(forms.ModelForm):
         fields = ['title', 'tag', 'category', 'body']
         widgets = {
             'title': forms.TextInput(attrs={"class": "form-control"}),
-            'tag': forms.CheckboxSelectMultiple(attrs={"class": "form-check"}),
+            'tag': forms.CheckboxSelectMultiple(),
             'category': forms.Select(attrs={"class": "form-select"}),
             'body': forms.Textarea(attrs={"class": "form-control"}),
         }
