d3321070476e111f61a6041a69ec654d87f63858
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-04-26 16:57:59 +0500

    add players

--- django/robocode/db_managment/db_managment/urls.py
+++ django/robocode/db_managment/db_managment/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,3 +24,8 @@ urlpatterns = [
     # path('posts/', include('articles.urls', namespace='articles')),
     path('', include('players.urls', namespace='players')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
