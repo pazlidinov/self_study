a6d4e818a025c999716b3b6472a168fcf307177a
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-08-21 11:05:52 +0500

    add viewsets and router

--- rest_api/todo_project/todo_app/urls.py
+++ rest_api/todo_project/todo_app/urls.py
@@ -1,15 +1,25 @@
 from django.urls import path
 from . import views
+# secend section
+from rest_framework.routers import SimpleRouter
 
 app_name = 'todo_app'
 
 
-urlpatterns = [
-    #for user
-    path('users/', views.UserList.as_view(), name='user_list'),
-    path('user/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
-    
-    #for posts
-    path('', views.ListTodo.as_view(), name='todo_list'),
-    path('<int:pk>/', views.DetailTodo.as_view(), name='todo_detail'),
-]
+# urlpatterns = [
+#     #for user
+#     path('users/', views.UserList.as_view(), name='user_list'),
+#     path('user/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+
+#     #for posts
+#     path('', views.ListTodo.as_view(), name='todo_list'),
+#     path('<int:pk>/', views.DetailTodo.as_view(), name='todo_detail'),
+# ]
+
+
+#secend section 
+router=SimpleRouter()
+router.register('users', views.UserViewSet,basename='users')
+router.register('posts', views.PostViewSet,basename='posts')
+
+urlpatterns =router.urls
--- rest_api/todo_project/todo_app/views.py
+++ rest_api/todo_project/todo_app/views.py
@@ -1,26 +1,46 @@
-from django.contrib.auth import get_user_model
-from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+# from django.contrib.auth import get_user_model
+# from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+# from .models import Todo
+# from .serializers import TodoSerializer, UserSerializer
+
+# secend section
+from rest_framework.viewsets import ModelViewSet
+from .permissions import IsAuthorOrReadOnly
 from .models import Todo
 from .serializers import TodoSerializer, UserSerializer
-
+from django.contrib.auth import get_user_model
+from .permissions import IsAuthorOrReadOnly
 # Create your views here.
 
 
-class ListTodo(ListCreateAPIView):
+# class ListTodo(ListCreateAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+
+
+# class DetailTodo(RetrieveDestroyAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+
+
+# class UserList(ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+# class UserDetail(RetrieveDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+# second section
+
+class PostViewSet(ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 
-class DetailTodo(RetrieveDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-
-
-class UserList(ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(RetrieveDestroyAPIView):
+class UserViewSet(ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
