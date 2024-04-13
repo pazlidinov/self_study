748f6698c7f5c62e7e542dae32126bc8963e9084
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2024-04-13 17:52:58 +0500

    complete RandomArticle.vue

--- vue_and_laravel/blog_backend/app/Http/Controllers/ArticleController.php
+++ vue_and_laravel/blog_backend/app/Http/Controllers/ArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Http\Request;
+
 
 class ArticleController extends Controller
 {
@@ -83,7 +85,7 @@ class ArticleController extends Controller
         //
     }
 
-    
+
     /**
      * Display a listing of random articles.
      *
@@ -92,7 +94,31 @@ class ArticleController extends Controller
      */
     public function random_articles()
     {
-       return Article::inRandomOrder()->with('categories')->limit(12)->get();
+        $articles = Article::inRandomOrder()->limit(12)->get();
+        $article_category_id = [];
+        foreach ($articles as $item) {
+            array_push($article_category_id, $item['category_id']);
+        };
+        $cat = Category::whereIn('id', $article_category_id)->get();
+        foreach ($articles as $article_item) {
+            foreach ($cat as $cat_item) {
+                if ($article_item['category_id'] == $cat_item['id']) {
+                    $article_item['category'] = $cat_item['name'];
+                }
+            }
+        };
+        return $articles;
+
+        // $cat = Category::where('id', 6)->first();
+        // Article::create(
+        //     [
+        //         'title' => 'article-6',
+        //         'photo' => 'C:\Users\XTreme.ws\Desktop\blog_photo\img-3.jpg',
+        //         'body' => 'irvnionvonv  rv wowov wwoif wo fw fwf wf wf iowr fiow foiwf oiw oiw oiw fowrim fwoi fowi fw frfoiwfiwf iowfoin foiwfiwr oir ow owoiw oiw oiw oiw fiw fowi fwrjiwrj wirffjifw fwf owjfwwj wojwj wr jw wj w w pw  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident molestiae, quasi officia ipsam nostrum quo totam dolores unde fugiat error molestias vero vel sed! Minima non cumque nesciunt nemo itaque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, obcaecati voluptate minima ullam nihil tenetur natus eaque temporibus iusto dolorum possimus quasi aliquid harum error! Eum odio vel nulla recusandae!',
+        //         'user_id' => '1',
+        //         'category_id' => $cat->id,
+        //     ]
+        // );
+        // return 'ok';
     }
-    
 }
--- vue_and_laravel/blog_frontend/src/components/RandomArticle.vue
+++ vue_and_laravel/blog_frontend/src/components/RandomArticle.vue
@@ -1,16 +1,16 @@
 <template>
-    
+
     <!-- Features Start -->
     <div class="container-fluid features mb-2">
         <div class="container py-5">
             <div class="row g-4">
-                <div v-for="article in articles" class="col-md-6 col-lg-6 col-xl-3">
+                <div v-for="article in first_articles" class="col-md-6 col-lg-6 col-xl-3">
                     <div class="row g-4 align-items-center features-item">
                         <div class="col-4">
                             <div class="rounded-circle position-relative">
                                 <div class="overflow-hidden rounded-circle">
-                                    <img v-bind:src="article.photo"
-                                        class="img-zoomin img-fluid rounded-circle w-100" alt="">
+                                    <img v-bind:src="article.photo" class="img-zoomin img-fluid rounded-circle w-100"
+                                        alt="">
                                 </div>
                                 <span
                                     class="rounded-circle border border-2 border-white bg-primary btn-sm-square text-white position-absolute"
@@ -19,16 +19,17 @@
                         </div>
                         <div class="col-8">
                             <div class="features-content d-flex flex-column">
-                                <p class="text-uppercase mb-2">{{ article.category_id }}</p>
+                                <p class="text-uppercase mb-2">{{ article.categories }}</p>
                                 <a href="#" class="h6">
-                                    {{ articles.title }}
+                                    {{ article.title }}
                                 </a>
-                                <small class="text-body d-block"><i class="fas fa-calendar-alt me-1"></i>{{ articles.created_at }}</small>
+                                <small class="text-body d-block"><i class="fas fa-calendar-alt me-1"></i>{{
+                                    article.created_at.slice(0, 10) }}</small>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     </div>
@@ -39,27 +40,25 @@
     <div class="container-fluid py-2">
         <div class="container py-5">
             <div class="row g-4">
-                <div class="col-lg-7 col-xl-8 mt-0">
+                <div v-for="article in second_articles" class="col-lg-7 col-xl-8 mt-0">
                     <div class="position-relative overflow-hidden rounded">
-                        <img src="" class="img-fluid rounded img-zoomin w-100" alt="">
+                        <img v-bind:src="article.photo" class="img-fluid rounded img-zoomin w-100" alt="">
                         <div class="d-flex justify-content-center px-4 position-absolute flex-wrap"
                             style="bottom: 10px; left: 0;">
-                            <a href="#" class="text-white me-3 link-hover"><i class="fa fa-clock"></i> 06 minute
-                                read</a>
-                            <a href="#" class="text-white me-3 link-hover"><i class="fa fa-eye"></i> 3.5k Views</a>
+                            <a href="#" class="text-white me-3 link-hover"><i class="fa fa-clock"></i> {{
+                               article.created_at.slice(0, 10) }}</a>
+                            <a href="#" class="text-white me-3 link-hover"><i class="fa fa-eye"></i> {{ article.view }} Views</a>
                             <a href="#" class="text-white me-3 link-hover"><i class="fa fa-comment-dots"></i> 05
                                 Comment</a>
-                            <a href="#" class="text-white link-hover"><i class="fa fa-arrow-up"></i> 1.5k Share</a>
+
                         </div>
                     </div>
                     <div class="border-bottom py-3">
-                        <a href="#" class="display-4 text-dark mb-0 link-hover">Lorem Ipsum is simply dummy text of the
-                            printing</a>
+                        <a href="#" class="display-4 text-dark mb-0 link-hover">
+                            {{ article.title }}
+                        </a>
                     </div>
-                    <p class="mt-3 mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                        printer took a galley standard dummy text ever since the 1500s, when an unknown printer took a
-                        galley...
+                    <p class="mt-3 mb-4">{{ article.body.slice(0,200) }}...
                     </p>
 
                 </div>
@@ -67,7 +66,7 @@
                     <div class="bg-light rounded p-4 pt-0">
                         <div class="row g-4">
 
-                            <div class="col-12">
+                            <div v-for="article in third_articles" class="col-12">
                                 <div class="row g-4 align-items-center">
                                     <div class="col-5">
                                         <div class="overflow-hidden rounded">
@@ -76,93 +75,15 @@
                                     </div>
                                     <div class="col-7">
                                         <div class="features-content d-flex flex-column">
-                                            <a href="#" class="h6">Get the best speak market, news.</a>
-                                            <small><i class="fa fa-clock"> 06 minute read</i> </small>
-                                            <small><i class="fa fa-eye"> 3.5k Views</i></small>
+                                            <a href="#" class="h6">{{ article.title }}</a>
+                                            <small><i class="fa fa-clock"> {{
+                                                article.created_at.slice(0,10) }}</i> </small>
+                                            <small><i class="fa fa-eye">  {{ article.view }} Views</i></small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <div class="row g-4 align-items-center">
-                                    <div class="col-5">
-                                        <div class="overflow-hidden rounded">
-                                            <img src="" class="img-zoomin img-fluid rounded w-100" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-7">
-                                        <div class="features-content d-flex flex-column">
-                                            <a href="#" class="h6">Get the best speak market, news.</a>
-                                            <small><i class="fa fa-clock"> 06 minute read</i> </small>
-                                            <small><i class="fa fa-eye"> 3.5k Views</i></small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row g-4 align-items-center">
-                                    <div class="col-5">
-                                        <div class="overflow-hidden rounded">
-                                            <img src="" class="img-zoomin img-fluid rounded w-100" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-7">
-                                        <div class="features-content d-flex flex-column">
-                                            <a href="#" class="h6">Get the best speak market, news.</a>
-                                            <small><i class="fa fa-clock"> 06 minute read</i> </small>
-                                            <small><i class="fa fa-eye"> 3.5k Views</i></small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row g-4 align-items-center">
-                                    <div class="col-5">
-                                        <div class="overflow-hidden rounded">
-                                            <img src="" class="img-zoomin img-fluid rounded w-100" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-7">
-                                        <div class="features-content d-flex flex-column">
-                                            <a href="#" class="h6">Get the best speak market, news.</a>
-                                            <small><i class="fa fa-clock"> 06 minute read</i> </small>
-                                            <small><i class="fa fa-eye"> 3.5k Views</i></small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row g-4 align-items-center">
-                                    <div class="col-5">
-                                        <div class="overflow-hidden rounded">
-                                            <img src="" class="img-zoomin img-fluid rounded w-100" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-7">
-                                        <div class="features-content d-flex flex-column">
-                                            <a href="#" class="h6">Get the best speak market, news.</a>
-                                            <small><i class="fa fa-clock"> 06 minute read</i> </small>
-                                            <small><i class="fa fa-eye"> 3.5k Views</i></small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row g-4 align-items-center">
-                                    <div class="col-5">
-                                        <div class="overflow-hidden rounded">
-                                            <img src="" class="img-zoomin img-fluid rounded w-100" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-7">
-                                        <div class="features-content d-flex flex-column">
-                                            <a href="#" class="h6">Get the best speak market, news.</a>
-                                            <small><i class="fa fa-clock"> 06 minute read</i> </small>
-                                            <small><i class="fa fa-eye"> 3.5k Views</i></small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -180,13 +101,20 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            articles: null,
+            first_articles: null,
+            second_articles: null,
+            third_articles: null,
         }
     },
     async mounted() {
         let domain = await axios.get("../../data/url.txt");
         let response = await axios.get(domain.data + "random_articles");
-        this.articles = await response.data
+        this.first_articles = await response.data.slice(0, 4)
+        this.second_articles = await response.data.slice(4, 5)
+        this.third_articles = await response.data.slice(4, -1)
+        console.log(this.first_articles)
+        console.log(this.second_articles)
+        console.log(this.third_articles)
     },
 };
 </script>
