e92a24570463f7de1d1f71a6ab0b37d5ff348676
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2024-04-22 18:19:15 +0500

    Update api.php

--- vue_and_laravel/blog_backend/routes/api.php
+++ vue_and_laravel/blog_backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\UserAuthController;
+use App\Http\Controllers\UserController;
 use App\Models\Article;
 use App\Models\Category;
+use App\Models\Comments;
+use App\Models\ReplayComments;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +24,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResources(['category'=>Category::class]);
-Route::apiResources(['article'=>Article::class]);
+Route::middleware('auth:sanctum')->group(function () {});
+
+Route::apiResources(['user' => UserController::class]);
+Route::apiResources(['category' => Category::class]);
+Route::apiResources(['article' => Article::class]);
+Route::apiResources(['comments' => Comments::class]);
+Route::apiResources(['replaycomments' => ReplayComments::class]);
 
 
 
